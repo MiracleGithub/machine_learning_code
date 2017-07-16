b8c862904cf2b58b3951c27a93ea7a5665b3fd70
Miracle <surenbhagat80@gmail.com>
2017-07-16 16:27:13 +0530

    code for curve-fitting in machine learning

--- polyfit_housing_price.py
+++ polyfit_housing_price.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-(C) 2016 Nikolay Manchev, London Machine Learning Study Group
-http://www.meetup.com/London-Machine-Learning-Study-Group/
-This work is licensed under the Creative Commons Attribution 4.0 International
-License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by/4.0/.
-"""
 
 import matplotlib.pyplot as plt
 import numpy as np
